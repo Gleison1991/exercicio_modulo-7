76c0ac32dce008787389d75d1836b55f4d212cec
gleison <gleisondeivison3@gmail.com>
2023-03-21 22:34:44 -0300

    exercicio campos numericos

---
+++ main.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('areas');
+const numeroA = document.getElementById('A');
+const numeroB = document.getElementById('B');
+const campoA = parseFloat(numeroA);
+const campoB = parseFloat(numeroB);
+let formEValido = false;
+
+function validaCampo(campoA,campoB) {
+    return campoA > campoB;
+}
+form.addEventListener('submit',function(e){
+    e.preventDefault ();
+}
+
+    const mensagemSucesso = `Resultado do campo B correto: <b> ${campoB.value} <b> é maior que o valor A: <b> ${campoA.value} <b>`;
+    const mensagemErro = `Resultado incorreto do valor B: <b> ${campoB.value}<b> é menor que o valor de A: <b> ${campoA.value}<b>`;
+
+    formEValido = validaCampo(campoA, campoB);
+    if (formEValido ==true) {
+        const containerMensagemSucesso = document.querySelector('.message-success');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'none';
+        campoA = '';
+        campoB = '';
+
+    } else {
+        if  (!formEValido){
+                const containerMensagemError = document.querySelector('.message-error');
+                containerMensagemError.innerHTML = mensagemErro;
+                containerMensagemError.style.display = 'none';
+        };
+    }
