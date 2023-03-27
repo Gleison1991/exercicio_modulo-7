8e03d32f8475a2c5333d39b3475a032f64d41192
gleison <gleisondeivison3@gmail.com>
2023-03-27 18:40:35 -0300

    tarefa do modulo 7

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const form = document.getElementById('areas');
-const numeroA = document.getElementById('A');
-const numeroB = document.getElementById('B');
-const campoA = parseFloat(numeroA);
-const campoB = parseFloat(numeroB);
 let formEValido = false;
 
 function validaCampo(campoA,campoB) {
-    return campoA > campoB;
+    return campoA>campoB
 }
 
 form.addEventListener('submit',function(e){
     e.preventDefault ();
 })
 
-    const mensagemSucesso = `Resultado do campo B correto: <b> ${campoB.value} <b> é maior que o valor A: <b> ${campoA.value} <b>`;
-    const mensagemErro = `Resultado incorreto do valor B: <b> ${campoB.value}<b> é menor que o valor de A: <b> ${campoA.value}<b>`;
+const numeroA = document.getElementById('A');
+const numeroB = document.getElementById('B');
+const campoA = parseFloat(numeroA.value);
+const campoB = parseFloat(numeroB.value);
+
+    const mensagemSucesso = `Resultado do campo B correto: <b> ${campoB} <b> é maior que o valor A: <b> ${campoA} <b>`;
+    const mensagemErro = `Resultado incorreto do valor B: <b> ${campoB}<b> é menor que o valor de A: <b> ${campoA}<b>`;
 
     formEValido = validaCampo(campoA, campoB);
+
     if (formEValido ==true) {
         const containerMensagemSucesso = document.querySelector('.message-success');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'none';
-        campoA = '';
-        campoB = '';
+        containerMensagemSucesso.style.display = 'block';
 
     } else {
-        if  (!formEValido){
-                const containerMensagemError = document.querySelector('.message-error');
+                const containerMensagemErro = document.querySelector('.message-erro');
                 containerMensagemError.innerHTML = mensagemErro;
-                containerMensagemError.style.display = 'none';
+                containerMensagemErro.style.display = 'block';
         };
-    }
